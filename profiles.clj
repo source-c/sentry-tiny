87b07309e91bfd31cbce2f9891fab269f7b9775d
MelKori <mieldo@gmail.com>
2021-01-23 21:28:04 +0200

    refactor profiles

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,3 @@
-{:dev      {:plugins [[lein-ancient "0.6.15"]]}
+{:dev      {:plugins []}
  :provided {:dependencies [[org.clojure/clojure "1.10.1"]]}
- :uberjar  {:aot :all :jvm-opts ["-Xmx1G"]}}
+ :jar      {:aot :all}}
