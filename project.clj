bae58a42fce78db94f8776c03622241640e7b4ee
source-c <source-c@users.noreply.github.com>
2021-09-04 00:04:25 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject net.tbt-post/sentry-tiny (-> "VERSION" slurp .trim)
   :description "Tiny Sentry Interface for Clojure"
   :url "https://github.com/source-c/sentry-tiny"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License 2.0"
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[cheshire "5.10.1"]
                  [http-kit "2.5.3"]])
